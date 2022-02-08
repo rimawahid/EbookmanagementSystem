4f31bb7b80a3211c5459215b4421777cadb1f734
Rima Akter <rimawahid3@gmail.com>
2022-02-08 23:23:28 +0600

    insert img

--- src/java/com/ebook/dao/BookDAO.java
+++ src/java/com/ebook/dao/BookDAO.java
@@ -7,17 +7,46 @@ package com.ebook.dao;
 
 import com.ebook.common.ICommonInterface;
 import com.ebook.model.Book;
+import com.ebook.util.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
+import javax.faces.bean.ManagedBean;
+
 
 /**
  *
  * @author USER
  */
-public class BookDAO implements ICommonInterface<Book>{
+@ManagedBean(name = "bookDAO")
+public class BookDAO implements ICommonInterface<Book> {
+
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
 
     @Override
+    
     public int save(Book t) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "insert into book_dtls (book_name, author, price, book_category, status, photo, email) values(?,?,?,?,?,?,?)";
+        int status = 0;
+        try {
+            con = DBConnection.getConnect();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, t.getBookName());
+            ps.setString(2, t.getAuthor());
+            ps.setDouble(3, t.getPrice());
+            ps.setString(4, t.getBookCategory());
+            ps.setString(5, t.getStatus());
+            ps.setString(6, t.getImg());
+            ps.setString(7, t.getEmail());
+            //System.out.println(t.getName());
+            ps.executeUpdate();
+        } catch (Exception e) {
+        }
+        return status;
     }
 
     @Override
@@ -42,7 +71,39 @@ public class BookDAO implements ICommonInterface<Book>{
 
     @Override
     public List<Book> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "select * from book_dtls";
+        ArrayList<Book> books = new ArrayList<Book>();
+        try {
+            con = DBConnection.getConnect();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            
+            while (rs.next()) {
+                Book showBook = new Book();
+//                System.out.println(rs.getString("name"));
+                showBook.setBookName(rs.getString("book_name"));
+                showBook.setAuthor(rs.getString("author"));
+                showBook.setPrice(rs.getDouble("price"));
+                showBook.setBookCategory(rs.getString("book_category"));
+                showBook.setStatus(rs.getString("status"));
+                showBook.setImg(rs.getString("photo"));
+                showBook.setEmail(rs.getString("email"));
+                showBook.setId(rs.getInt("id"));
+                books.add(showBook);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+                ps.close();
+                rs.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return books;
     }
-    
+
 }
--- src/java/com/ebook/model/Book.java
+++ src/java/com/ebook/model/Book.java
@@ -1,15 +1,33 @@
-
 package com.ebook.model;
 
+import com.ebook.dao.BookDAO;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.servlet.http.Part;
 
 @ManagedBean
 @RequestScoped
 public class Book {
- private int id;
- private String bookName,author, bookCategory, status, photoName, email;
- private double price;
+
+    private int id;
+    private String bookName, author, bookCategory, status, email, img;
+    private double price;
+    private Part file;
+    ArrayList books;
+
+    public ArrayList getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList books) {
+        this.books = books;
+    }
 
     public int getId() {
         return id;
@@ -51,14 +69,6 @@ public class Book {
         this.status = status;
     }
 
-    public String getPhotoName() {
-        return photoName;
-    }
-
-    public void setPhotoName(String photoName) {
-        this.photoName = photoName;
-    }
-
     public String getEmail() {
         return email;
     }
@@ -74,8 +84,32 @@ public class Book {
     public void setPrice(double price) {
         this.price = price;
     }
- 
-    public Book() {
+
+    public Part getFile() {
+        return file;
     }
+
+    public void setFile(Part file) {
+        this.file = file;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
     
+
+    public void addBook() throws IOException {
+        String fileName = Paths.get(file.getSubmittedFileName()).getFileName().toString();
+        File savedFile = new File("D:\\IDB\\Project\\EbookManagementSystem\\web\\book\\",fileName);
+        InputStream input = file.getInputStream();
+        Files.copy(input, savedFile.toPath());
+        //img = "img/" + fileName;
+        img = "/book/" + fileName; 
+        new BookDAO().save(this);
+    }
 }
